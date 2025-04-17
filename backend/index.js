4089a5c4de72efc61b416ef09b4506f26c6a2bde
shivam kumar <shivamsofteng@gmail.com>
2025-04-17 22:38:23 +0530

    bug fixed

--- backend/index.js
+++ backend/index.js
@@ -23,19 +23,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
 const PORT = process.env.PORT || 3000;
 
 
 // api's
-app.get("/",(req,res)=>{
-    res.send("all working")
-})
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.get("/",(req,res)=>{
+    res.send("all working")
+})
 
 
 app.listen(PORT,()=>{
