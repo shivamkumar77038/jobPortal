2bb7acb867887a8486bb13763720502c3a7d64b7
shivam kumar <shivamsofteng@gmail.com>
2025-04-17 22:51:24 +0530

    db modified

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+let isConnected = false;  // Flag to track the database connection
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;  // Set the connection flag to true once connected
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);  // Exit the process if connection fails
+  }
+};
+
 export default connectDB;
