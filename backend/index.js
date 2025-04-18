00adfc5ae40ff0fe0945b70cd8639fb7d0a01fef
shivam kumar <shivamsofteng@gmail.com>
2025-04-18 21:38:12 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // update to match frontend URL if hosted
+    origin: 'https://job-portalfrontend-ruby.vercel.app', // update to match frontend URL if hosted
     credentials: true
 };
 app.use(cors(corsOptions));
